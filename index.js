c22278efb3761aed8fd3c5365746fb81cac7d200
Michelle Kim <pasolre95kim@gmail.com>
2018-06-14 19:00:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function johnLennonFacts(facts) {
 
 function iLoveTheBeatles(number) {
   var array = [];
-  var i = 0;
+  var number = 0;
   do {
     array.push("I love the Beatles!");
     i++;
